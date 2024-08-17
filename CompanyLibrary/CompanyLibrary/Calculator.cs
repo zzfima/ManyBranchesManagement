7d3a275934f54e549c71408e703ce0817a851d04
zzfima <zzfima@gmail.com>
2024-08-17 10:29:40 +0300

    fix adder

--- CompanyLibrary/CompanyLibrary/Calculator.cs
+++ CompanyLibrary/CompanyLibrary/Calculator.cs
@@ -2,7 +2,7 @@
 {
     public class Calculator
     {
-        public int Adder(int x, int y) => x + y + 1;
+        public int Adder(int x, int y) => x + y;
         public int Mult(int x, int y) => x * y + 1;
     }
 }
