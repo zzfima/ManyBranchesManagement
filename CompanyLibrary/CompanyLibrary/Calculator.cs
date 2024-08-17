1387608fb02a6535240cafd1bd6f5a441d8c18e1
zzfima <zzfima@gmail.com>
2024-08-17 10:19:59 +0300

    fix bug

--- CompanyLibrary/CompanyLibrary/Calculator.cs
+++ CompanyLibrary/CompanyLibrary/Calculator.cs
@@ -3,6 +3,6 @@
     public class Calculator
     {
         public int Adder(int x, int y) => x + y + 1;
-        public int Multiplier(int x, int y) => x * y + 1;
+        public int Multiplier(int x, int y) => x * y;
     }
 }
