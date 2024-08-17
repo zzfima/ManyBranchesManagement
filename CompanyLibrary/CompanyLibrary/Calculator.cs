55be6cf062dad1330939ef71837832843a382d35
zzfima <zzfima@gmail.com>
2024-08-17 10:21:35 +0300

    make 2 bugs

--- CompanyLibrary/CompanyLibrary/Calculator.cs
+++ CompanyLibrary/CompanyLibrary/Calculator.cs
@@ -2,6 +2,7 @@
 {
     public class Calculator
     {
-        public int Adder(int x, int y) => x + y;
+        public int Adder(int x, int y) => x + y + 1;
+        public int Mult(int x, int y) => x * y + 1;
     }
 }
